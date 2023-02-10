2c1f564f2c57a8b180d414e6509b3994f4d81002
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-10 10:54:01 +0530

    bitwise operator compliment

---
+++ Bitwise operators/op_complement.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of a ",a);
+    scanf("%d",&a);
+    printf("enter the value of b ",b);
+    scanf("%d",&b);
+    printf("%d compliment of b is ",~b);
+    return 0;
+}
