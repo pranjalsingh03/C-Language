d06b066fdc6141f278536cb0f738347285078a4f
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-10 10:28:32 +0530

    converting kg to gram

---
+++ Questions/convert_kg_gm.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,convert;
+    printf("Enter value in Kg:%d ",a);
+    scanf("%d",&a);
+    convert=a*1000;
+    printf("%dKg to gram conversion is: %d gms",a,convert);
+}
