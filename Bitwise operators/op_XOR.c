2ee81c138e0f22adcbb49d8c1df64e15992915a9
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-10 10:51:01 +0530

    bitwise operator XOR

---
+++ Bitwise operators/op_XOR.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of a ",a);
+    scanf("%d",&a);
+    printf("enter the value of b ",b);
+    scanf("%d",&b);
+    printf("%d",a^b);
+}
