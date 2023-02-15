b0c2b5bf47d54c3d32bbb1ec4426926a4eed23f0
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-15 11:58:41 +0530

    question 4

--- Questions/ques4.c
+++ Questions/ques4.c
@@ -1,2 +1,15 @@
 // Write a programe to check weather entered number is divisible by both 5 and 2
 
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the number ");
+    scanf("%d",&a);
+    if (a%5==0 &&a%2==0)
+    printf("%d is divisible by both 5 and 2",a);
+    else
+    printf("%d is not divisible by both 5 and 2",a);
+
+
+}
