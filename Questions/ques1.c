5d376e2a6318a79a5637a85ba475b370a338dd2a
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-15 11:56:49 +0530

    question 1

--- Questions/ques1.c
+++ Questions/ques1.c
@@ -1,1 +1,14 @@
 // Write a program to check weather the enter number is even or odd number
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the number ");
+    scanf("%d",&a);
+    if (a%2==0)
+    printf("%d is even number",a);
+    else
+    printf("%d is odd nuuber ",a);
+    return 0;
+}
--- Questions/ques4.c
+++ Questions/ques4.c
@@ -1,1 +1,2 @@
 // Write a programe to check weather entered number is divisible by both 5 and 2
+
