d593c8c9afc0c3439699882683e46a142f6cfb13
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-15 11:57:34 +0530

    question 2

--- Questions/ques2.c
+++ Questions/ques2.c
@@ -1,1 +1,13 @@
 //Write a programe to check weather the entered chareacter is vowel or not 
+
+#include<stdio.h>
+
+int main(){
+    char a;
+    printf("Enter the character ");
+    scanf("%c",&a);
+    if (a=='a'||a=='A'||a=='E'||a=='e'||a=='I'||a=='i'||a=='O'||a=='o'||a=='U'||a=='u')
+    printf("Enter character is vowel");
+    else
+    printf("Enter character is not vowel");
+}
