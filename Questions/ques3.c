6087e249b7509420af2f857b496159f72b682e24
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-15 11:58:17 +0530

    question 3

--- Questions/ques3.c
+++ Questions/ques3.c
@@ -1,1 +1,15 @@
 // Write a programe to check weather entered number is divisible by 2 and 8 both 
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the number ");
+    scanf("%d",&a);
+    if (a%2==0 &&a%8==0)
+    printf("%d is divisible by both 2 and 8",a);
+    else
+    printf("%d is not divisible by both 2 and 8",a);
+
+
+}
