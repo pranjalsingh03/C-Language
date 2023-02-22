16bb7d65317210a5e1273ab0803ac5c592dc5f0d
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-22 11:21:12 +0530

    for loop in c

---
+++ Loops/forloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a ,n;
+    printf("enter the  number");
+    scanf("%d",&n);
+    for(a=1 ; a<=n; a++){
+        printf("%d",a);
+
+    }
+    return 0;
+}
