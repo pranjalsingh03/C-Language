4fdff9876685f1727d65d525bf12532c56a8c36a
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-24 23:47:04 +0530

    day 3 in c

--- Class/day3.c
+++ Class/day3.c
@@ -21,8 +21,26 @@ printf("CROP IT\n");
 }
 }
 
-/* Enter your code here. Read input from STDIN. Print output to STDOUT */
 return 0;
 }
 
-9:35 AM
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int l,r,k,i=0,count=0;
+scanf("%d%d%d",&l,&r,&k);
+for(i=l;i<=r;i++)
+{
+if(i%k==0)
+count++;
+}
+printf("%d",count);
+
+return 0;
+}
