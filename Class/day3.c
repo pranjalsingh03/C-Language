688d29b6fe06359ff70d97b2ab5c69f15e12ad9f
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-24 23:45:48 +0530

    day 3

---
+++ Class/day3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int l,n,w,h,i;
+scanf("%d",&l);
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d%d",&w,&h);
+if(w<l||h<l)
+printf("UPLOAD ANOTHER\n");
+else
+{
+if(w==h)
+printf("ACCEPTED\n");
+else
+printf("CROP IT\n");
+}
+}
+
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+return 0;
+}
+
+9:35 AM
