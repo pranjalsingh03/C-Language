669e5ebea87526d88b04c8257abd3d15a4c1736f
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-26 22:36:18 +0530

    day 4 n c

--- Class/day4.c
+++ Class/day4.c
@@ -29,3 +29,41 @@ else
 printf("invalid choice");
 return 0;
 }
+
+
+
+#include<stdio.h>
+int main()
+{
+int a,b,res;
+int choice;
+printf("Press 1 for add\n");
+printf("Press 2 for sub\n");
+printf("Press 3 for mul\n");
+printf("enter your choice=");
+scanf("%d",&choice);
+if(choice==1)
+{
+printf("enter your inputs=");
+scanf("%d%d",&a,&b);
+res=a+b;
+printf("sum=%d",res);
+}
+else if(choice==2)
+{
+printf("enter your inputs=");
+scanf("%d%d",&a,&b);
+res=a-b;
+printf("sub=%d",res);
+}
+else if(choice==3)
+{
+printf("enter your inputs=");
+scanf("%d%d",&a,&b);
+res=a*b;
+printf("mul=%d",res);
+}
+else
+printf("invalid choice");
+return 0;
+}
