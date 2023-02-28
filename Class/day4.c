fb763346afe35d992a82d2d82764f14841a7fced
Pranjal Singh <112399218+pranjalsingh03@users.noreply.github.com>
2023-02-28 22:18:36 +0530

    day 4 in c language

--- Class/day4.c
+++ Class/day4.c
@@ -176,3 +176,47 @@ i++;
 
 return 0;
 }
+
+
+
+#include<stdio.h>
+int main()
+{
+int i=1;
+do
+{
+printf("%d\n",i);
+i++;
+}while(i<5);
+
+return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+int i=5;
+do
+{
+printf("%d\n",i);
+i--;
+}while(i>0);
+
+return 0;
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+int i,n;
+printf("enter the value=");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",i);
+}
+
+return 0;
+}
