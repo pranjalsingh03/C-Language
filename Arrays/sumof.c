b7bb4121292d2242bc562590569f95f36136e637
Pranjal Singh <pranjalsingh9304@gmail.com>
2023-03-22 11:56:50 +0530

    sum of array in c

--- Arrays/sumof.c
+++ Arrays/sumof.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int a[7] ,i , n;
+    int a[7] ,i , n, sum;
     printf("Enter the  number of elements");
     scanf("%d",&n);
     printf("\n Enter the elements");
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
+        sum = sum+ a[i];
     }
-    printf("\n The elements are\n");
-    for(i=0;i<n;i++){
-        printf("%d\t%u\n",a[i],&a[i]);
+    printf("Sum of the array =%d",sum);
     }
-}
