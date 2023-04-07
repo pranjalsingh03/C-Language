43e90b6c48730ce96155eb7b02b317ae4d615d97
Pranjal Singh <pranjalsingh9304@gmail.com>
2023-04-07 09:40:43 +0530

    2D array

---
+++ Arrays/2D_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int a[3][3], i , j;
+    for (i=0; i<3;i++){
+        for (j=0;j<3;j++){
+            printf("enter the value of a[%d][%d]: " ,i,j);
+            scanf("%d", &a[i][j]);
+
+        }
+    }
+    printf("elements of 2D matrix are \n");
+    for (i=0;i<3;i++){
+        for (j=0;j<3;j++){
+            printf("%d  %u\t" , a[i][j], &a[i][j]);  // to print address also
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
